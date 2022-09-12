525fb447c6fbd11928e5860c7d89728c10464b9c
RC2PC2 <petriccarcc@gmail.com>
2022-09-12 12:05:10 +0200

    Revert "added script src"
    This reverts commit b69dec194c3fc78409949f7077456b56286c58d5.

--- index.php
+++ index.php
@@ -7,7 +7,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Test GIT</title>
     <link rel="stylesheet" href="./style/style.css">
-    <script src="./script/script.js"></script>
 </head>
 <body>
 
