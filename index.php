b69dec194c3fc78409949f7077456b56286c58d5
RC2PC2 <petriccarcc@gmail.com>
2022-09-12 12:03:22 +0200

    added script src

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Test GIT</title>
     <link rel="stylesheet" href="./style/style.css">
+    <script src="./script/script.js"></script>
 </head>
 <body>
 
